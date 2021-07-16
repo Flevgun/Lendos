34f1213b700f38ece86add5a20ff66c698583399
Flevgun Flevgun <sansan0851.sk@gmail.com>
2021-07-16 23:47:21 +0500

    first

---
+++ src/components/Input.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      <label :for="labelfor">Гей:</label>
+      <input :type="inputType" :name="inputName" :id="inputId">
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
